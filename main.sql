6abcc763af19429137670b507d4a1a795184090d
Madhusha Prasad <50085447+MadhushaPrasad@users.noreply.github.com>
2022-08-08 20:22:42 +0530

    # commit: create stock table

--- main.sql
+++ main.sql
@@ -3,4 +3,11 @@ CREATE TABLE client(
     name VARCHAR(12),
     address VARCHAR(30),
     PRIMARY KEY (clno)
+) / 
+CREATE TABLE stock(
+    company CHAR(7),
+    price NUMBER(6, 2),
+    dividend NUMBER(4, 2),
+    eps NUMBER(4, 2),
+    PRIMARY KEY (company)
 ) /
