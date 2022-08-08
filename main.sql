4bada73fee53a84b275dbbec86fb89828199e7f5
Madhusha Prasad <50085447+MadhushaPrasad@users.noreply.github.com>
2022-08-08 20:25:19 +0530

    #commit : create trading table with foreign key

--- main.sql
+++ main.sql
@@ -4,10 +4,18 @@ CREATE TABLE client(
     address VARCHAR(30),
     PRIMARY KEY (clno)
 ) / 
+
 CREATE TABLE stock(
     company CHAR(7),
     price NUMBER(6, 2),
     dividend NUMBER(4, 2),
     eps NUMBER(4, 2),
     PRIMARY KEY (company)
+) / 
+
+CREATE TABLE trading(
+    company CHAR(7),
+    exchange VARCHAR(12),
+    PRIMARY KEY (company, exchange),
+    FOREIGN KEY (company) REFERENCES stock(company)
 ) /
