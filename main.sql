0e03b5bbb1eee77e4de0c362a7735cd253932498
Madhusha Prasad <50085447+MadhushaPrasad@users.noreply.github.com>
2022-08-08 20:20:20 +0530

    #commit : set primary key

--- main.sql
+++ main.sql
@@ -1,1 +1,6 @@
-CREATE TABLE client(clno CHAR(3), name VARCHAR(12), address VARCHAR(30)) /
+CREATE TABLE client(
+    clno CHAR(3),
+    name VARCHAR(12),
+    address VARCHAR(30),
+    PRIMARY KEY (clno)
+) /
