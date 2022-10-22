f4c9efab4ee0d92be45fd6c0ab5536ad977dd1ad
Madhusha Prasad <50085447+MadhushaPrasad@users.noreply.github.com>
2022-10-22 12:00:47 +0530

    path expression wild card example 01

--- xml_database.sql
+++ xml_database.sql
@@ -14,6 +14,8 @@
 -- DTD
 -- XML Schema
 -- Relax NG
+------------------------------------------------------------------------
+-- insertion of xml data and Xml data type
 
 CREATE TABLE adminUsers(
 	id int NOT NULL PRIMARY KEY,
@@ -54,8 +56,9 @@ INSERT INTO adminUsers VALUES(3,
 	'
 );
 
-
+-------------------------------------------------------------------
 -- path expression
+
 -- Simple path descriptors are sequences of
 -- tags separated by slashes (/).
 
@@ -83,8 +86,7 @@ INSERT INTO adminUsers VALUES(3,
             <PRICE theBeer = “Bud”>2.50</PRICE>
             <PRICE theBeer = “Miller”>3.00</PRICE>
     </BAR>
-    <BEER name = “Bud” soldBy = “JoesBar
-    SuesBar/>
+    <BEER name = “Bud” soldBy = “JoesBar SuesBar/>
 </BARS>
 
     -- /BARS/BAR/PRICE describes the
@@ -100,13 +102,33 @@ INSERT INTO adminUsers VALUES(3,
             <PRICE theBeer = “Bud”>2.50</PRICE>
             <PRICE theBeer = “Miller”>3.00</PRICE>
         </BAR>
-        <BEER name = “Bud” soldBy = “JoesBar
-        SuesBar …”/>
+        <BEER name = “Bud” soldBy = “JoesBar SuesBar …”/>
     </BARS>
 
 -- //PRICE describes the PRICE
 -- Elements to appear within
 -- the document.
+
+-- Wild Card *
+
+-- A star (*) in place of a tag represents any one
+-- tag.
+
+-- Example: /*/*/PRICE represents all price
+-- objects at the third level of nesting.
+-------------------------------------------------------------------------
+-- Example 01: /BARS/*
+    <BARS>
+        <BAR name = “JoesBar”>
+            <PRICE theBeer = “Bud”>2.50</PRICE>
+            <PRICE theBeer = “Miller”>3.00</PRICE>
+        </BAR>
+        <BEER name = “Bud” soldBy = “JoesBar SuesBar”/>
+    </BARS>
+
+-- /BARS/* captures all BAR
+-- and BEER elements, such
+-- as these.
 
 /*same code in the xql Database*/
 SELECT id,xDoc.query('/*/doc[@id="123"]') FROM adminUsers;
